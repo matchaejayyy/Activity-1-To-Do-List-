99ea2e89fde58d881d973ae95561ee8c01294b3a
matchaejayyy <145851990+matchaejayyy@users.noreply.github.com>
2024-09-03 21:34:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const C = document.getElementById('tasks');
 
 function addTask(event) {
     event.preventDefault();
-    if (B.value === '') return;
-    const task = createTask(B.value);
-    C.appendChild(task);
-    B.value = '';
+    if (B.value === '') {
+        alert("Please enter a task")
+        return;
+    }
+    else {
+        const task = createTask(B.value);
+        C.appendChild(task);
+        B.value = '';
+    }
 }
 
 
@@ -21,12 +26,26 @@ function createTask(taskName) {
          <span class="delete">&times;</span> <!-- delete button -->
     `;
 
+    check = false
+
+    task.addEventListener("click", () => {
+        check = !check
+
+        if (check) {
+            task.style.textDecoration = "line-through";
+        } if (!check) {
+            task.style.textDecoration = "none";
+        }
+
+    });
 
     const deleteButton = task.querySelector('.delete');
     deleteButton.addEventListener('click', deleteTask);
     return task;
 }
-
+A.addEventListener("click", () => {
+    A.classlist.toggle("checked")
+})
 
 function deleteTask(event) {
     event.target.parentElement.remove();
